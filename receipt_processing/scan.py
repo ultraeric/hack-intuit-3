c4c77bf1f06f016364b86abf31ba48d5d6079941
Jonathan Tan <wawadragaan@gmail.com>
2017-09-16 08:35:36 -0700

    working

--- receipt_processing/scan.py
+++ receipt_processing/scan.py
@@ -4,7 +4,6 @@ import pytesseract
 import regex as re
 import socket
 import sys
-import spell
 from requests import get
 
 try:
@@ -20,12 +19,12 @@ def processImage(url):
             response = get(url)
             file.write(response.content)
 
-    download(url, 'image')
+    download(url, '/tmp/image')
 
     regex = re.compile('.*[0-9]+\.|,[0-9]+.*')
     r = re.compile('(.*TOTAL.*){s<=2}')
 
-    receipt = pytesseract.image_to_string(Image.open('image'), config = '-psm 6').splitlines()
+    receipt = pytesseract.image_to_string(Image.open('/tmp/image'), config = '-psm 6').splitlines()
     matches = [string for string in receipt if re.match(regex, string)]
     total = [string for string in receipt if re.match(r, string)]
 
@@ -39,7 +38,6 @@ def processImage(url):
 
     prices = []
     for tote in total:
-        print(tote)
         for word in tote.split():
             if re.match(regex, word):
                 try:
@@ -55,7 +53,7 @@ def processImage(url):
         total['total'] = max(prices)
     except:
         #print("Couldn't find a total.")
-        return None
+        pass
     return json.dumps(total)
 
 print(processImage(sys.argv[1]))
