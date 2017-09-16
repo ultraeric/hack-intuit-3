81a68df32b363e190ebf41ffc0b920858f4f6804
Eric (Yiqi) Hou <erichou2004@gmail.com>
2017-09-16 08:48:08 -0700

    Sync

--- decisionHandling/decisionHandler.js
+++ decisionHandling/decisionHandler.js
@@ -1,6 +1,11 @@
 import processing from './processing';
 function decisionHandler(id, msg, db) {
-
+  let result = processing(id, msg, db);
+  if (result) {
+    return result;
+  } else {
+    /* JT your stuff here! */
+  }
 }
 export default decisionHandler;
 export {decisionHandler};
