20b53b0fcadecb775e52c20106b413dd981686e7
Eric (Yiqi) Hou <erichou2004@gmail.com>
2017-09-16 10:13:14 -0700

    Merge

--- decisionHandling/decisionHandler.js
+++ decisionHandling/decisionHandler.js
@@ -1,6 +1,7 @@
 import processing from './processing';
 function decisionHandler(id, msg, db, sendTextMessage) {
-  let result = processing(id, msg, db, sendTextMessage);
+  // let result = processing(id, msg, db, sendTextMessage);
+  let result = null;
   if (result) {
     return result;
   } else {
