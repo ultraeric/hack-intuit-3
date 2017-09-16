51ee45d1611684bb075cfb908711ea6ab4b11ce9
Jonathan Tan <wawadragaan@gmail.com>
2017-09-16 08:52:34 -0700

    Initial skeleton for decisions

--- decisionHandling/decisionHandler.js
+++ decisionHandling/decisionHandler.js
@@ -2,5 +2,31 @@ import processing from './processing';
 function decisionHandler(id, msg, db) {
 
 }
+
+// Return list of possible queries/methods
+function help(id, msg) {
+
+}
+
+// Returns financial advice!
+function advise(id, msg) {
+
+}
+
+// Returns your progress to your financial goal
+function goal(id, msg, db) {
+
+}
+
+// Manually log a user's transaction
+function log(id, msg, db) {
+
+}
+
+// Return average amount spent per day
+function spent(id, msg, db) {
+
+}
+
 export default decisionHandler;
 export {decisionHandler};
